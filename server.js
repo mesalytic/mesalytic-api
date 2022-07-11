68aa2ebd3304c79c5b75c10a0dd60e696e1aeed0
Alice <atamatori56@gmail.com>
2022-07-11 12:52:04 +0200

    intern: endpoints w/o url requirement works with no url

--- server.js
+++ server.js
@@ -33,10 +33,14 @@ app.get('/', (req, res) => {
 
 app.use('/:endpoint', async (req, res) => {
     console.log(req.query);
-    if (req.query.url === undefined) return res.status(400).send({ success: false, message: "Bad Request 400: No specified image url", data: null })
-    if (!isImageUrl(req.query.url)) return res.status(400).send({ success: false, message: "Bad Request 400: Invalid image url", data: null })
-
-    require(`./endpoints/${req.params.endpoint}.js`)(req, res);
+    if (["fractal"].includes(req.params.endpoint)) require(`./endpoints/${req.params.endpoint}.js`)(req, res);
+    else {
+        if (req.query.url === undefined) return res.status(400).send({ success: false, message: "Bad Request 400: No specified image url", data: null })
+        if (!isImageUrl(req.query.url)) return res.status(400).send({ success: false, message: "Bad Request 400: Invalid image url", data: null })
+    
+        require(`./endpoints/${req.params.endpoint}.js`)(req, res);
+    
+    }
 })
 
 app.get('*', (req, res) => {
