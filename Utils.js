448d237f2da32ddfc0f46b006cc0c6892c0a23c8
Alice <atamatori56@gmail.com>
2022-07-12 16:10:50 +0200

    feat: add distort endpoint

--- Utils.js
+++ Utils.js
@@ -164,4 +164,27 @@ module.exports = class Utils {
 
         return { x, y, width, height }
     }
+
+    static distort(ctx, amplitude, x, y, width, height, strideLevel = 4) {
+        const data = ctx.getImageData(x, y, width, height);
+        const temp = ctx.getImageData(x, y, width, height);
+        const stride = width * strideLevel;
+
+        for (let i = 0; i < width; i++) {
+            for (let j = 0; j < height; j++) {
+                const xs = Math.round(amplitude * Math.sin(2 * Math.PI * 3 * (j / height)));
+                const ys = Math.round(amplitude * Math.cos(2 * Math.PI * 3 * (i / width)));
+
+                const dest = (j * stride) + (i * strideLevel);
+                const src = ((j + ys) * stride) + ((i + xs) * strideLevel);
+
+                data.data[dest] = temp.data[src];
+                data.data[dest + 1] = temp.data[src + 1];
+                data.data[dest + 2] = temp.data[src + 2];
+            }
+        }
+
+        ctx.putImageData(data, x, y);
+        return ctx;
+    }
 }
---
+++ endpoints/distort.js
@@ -0,0 +1,19 @@
+const router = require('express').Router();
+
+const Canvas = require('@napi-rs/canvas');
+const request = require('node-superfetch');
+const { distort } = require('../utils');
+
+module.exports = async (req, res) => {
+    const { body } = await request.get(req.query.url);
+    const avatar = await Canvas.loadImage(body);
+
+    const canvas = Canvas.createCanvas(avatar.width, avatar.height);
+    const ctx = canvas.getContext('2d');
+
+    ctx.drawImage(avatar, 0, 0);
+    distort(ctx, 15, 0, 0, avatar.width, avatar.height);
+
+    res.header('Content-Type', 'application/json');
+    res.send({ success: true, body: new Buffer.from(canvas.toBuffer(), 'base64'), ext: '.png'})
+}
