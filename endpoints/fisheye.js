224e73a79e24751497e4ea9514283ed9e39ef766
Alice <atamatori56@gmail.com>
2022-08-21 02:19:31 +0200

    feat: add fisheye endpoint

---
+++ endpoints/fisheye.js
@@ -0,0 +1,19 @@
+const router = require('express').Router();
+
+const Canvas = require('canvas');
+const request = require('node-superfetch');
+const { fishEye } = require('../Utils');
+
+module.exports = async (req, res) => {
+    const { body } = await request.get(req.query.url);
+    const avatar = await Canvas.loadImage(body);
+    
+    const canvas = Canvas.createCanvas(avatar.width, avatar.height);
+    const ctx = canvas.getContext('2d');
+
+    ctx.drawImage(avatar, 0, 0);
+    fishEye(ctx, 66, 0, 0, avatar.width, avatar.height)
+
+    res.header('Content-Type', 'application/json');
+    res.send({ success: true, body: new Buffer.from(canvas.toBuffer(), 'base64'), ext: '.png'})
+}
